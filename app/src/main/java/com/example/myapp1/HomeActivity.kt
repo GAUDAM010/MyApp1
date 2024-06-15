1c3274f0f26349a83c2905be777b594f8037968c
GAUDAM010 <gaudamvaibav.e2022@vitstudent.ac.in>
2024-06-15 19:55:04 +0530

    android app 1 updates

--- app/src/main/java/com/example/myapp1/HomeActivity.kt
+++ app/src/main/java/com/example/myapp1/HomeActivity.kt
@@ -4,22 +4,25 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
+import android.widget.EditText
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
 class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     //lateinit var brake:Int?
     var TAG = HomeActivity::class.java.simpleName   //"HomeActivity"
 
     var data = arrayOf("india","english","android","computers")
+    var b = 20
 
     lateinit var mySpinner: Spinner
     lateinit var recyclerview:RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var a = 10
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
         mySpinner = findViewById(R.id.spinner)
@@ -41,14 +44,24 @@ class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
            homeTextView.setText(data)
   */
     }
-
     override fun onItemSelected(adapter: AdapterView<*>?, view: View?, position: Int, id: Long) {
         var item = adapter?.selectedItem.toString()
         Log.i(TAG,item)
+        println(b)
+
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
         TODO("Not yet implemented")
+    }
+
+    fun getShowText(view: View) {
+        //get the text from et
+        var uiEt:EditText = findViewById(R.id.etUi)
+        var textTyped = uiEt.text.toString()
+        //show the text in the tv
+        var uiTv: TextView = findViewById(R.id.tvUi)
+        uiTv.setText(textTyped)
     }
 
 }
