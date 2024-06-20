2b5fbf5c22659ce2a8618db836b7eccabf6afc33
GAUDAM010 <gaudamvaibav.e2022@vitstudent.ac.in>
2024-06-20 19:03:46 +0530

    android app 1 updates

--- app/src/main/java/com/example/myapp1/network/MarsPhoto.kt
+++ app/src/main/java/com/example/myapp1/network/MarsPhoto.kt
@@ -1,15 +1,17 @@
 package com.example.myapp1.network
 
+import com.squareup.moshi.Json
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+
 
 //serialization -- deflate big baloon carry it.. deserialzation- inflate it back
 //autocad- no cd , floppy drive-- 1.44MB, mark sl no 1,2..
 //java obj into storeable format file
 
-@Serializable
+//work breakdown structure --tasks --independency testable, discrete ,small
 data class MarsPhoto(
     val id: String,
-    @SerialName(value = "img_src")
+    @Json(name = "img_src")
     val imgSrc: String
 )
