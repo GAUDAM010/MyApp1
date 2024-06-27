c964c486e2d713814c0db18253c0bfdc69c40175
GAUDAM010 <gaudamvaibav.e2022@vitstudent.ac.in>
2024-06-27 20:10:27 +0530

    android app 1 updates

--- app/src/main/java/com/example/myapp1/HomeActivity.kt
+++ app/src/main/java/com/example/myapp1/HomeActivity.kt
@@ -7,12 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 class HomeActivity : AppCompatActivity() {
     //lateinit var brake:Int?
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+
 
     }
 
