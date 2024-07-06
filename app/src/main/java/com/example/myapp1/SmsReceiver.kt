5879be847820aae89932f9b4b4cef401680c5cdf
GAUDAM010 <gaudamvaibav.e2022@vitstudent.ac.in>
2024-07-06 19:03:36 +0530

    Sms broadcast receiver

---
+++ app/src/main/java/com/example/myapp1/SmsReceiver.kt
@@ -0,0 +1,21 @@
+package com.example.myapp1
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.provider.Telephony
+import android.util.Log
+
+class SmsReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        val message = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+        val content =  message[0].displayMessageBody
+        // if(content.startsWith("command-- get location")) //send sms to the theif's phno
+
+
+        val phno = message[0].displayOriginatingAddress
+        Log.i("SmsReceiver","hi abdul you've an sms"+ content+phno)
+    }
+}
